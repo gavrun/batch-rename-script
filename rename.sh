6545a7589c82011058bac110100057e48d1d6316
gavrun <38539655+gavrun@users.noreply.github.com>
2022-12-04 11:28:35 +0300

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#echo "Screenshot_20221203_16:41:40.png" | sed 's/:/_/g'
+#Screenshot_20221203_16_41_40.png
+
+for i in *
+	do
+	    j=$(echo "$i" | sed 's/:/_/g')
+	    #echo mv \"$i\" \"$j\"
+	    mv \"$i\" \"$j\"
+	done
+  
