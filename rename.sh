95f624f7ab36554a59275b6df03dfb45302457f1
gavrun <38539655+gavrun@users.noreply.github.com>
2022-12-04 11:35:00 +0300

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -3,10 +3,9 @@
 #echo "Screenshot_20221203_16:41:40.png" | sed 's/:/_/g'
 #Screenshot_20221203_16_41_40.png
 
-for i in *
-	do
-	    j=$(echo "$i" | sed 's/:/_/g')
-	    #echo mv \"$i\" \"$j\"
-	    mv \"$i\" \"$j\"
-	done
-  
+for i in * 
+do
+    j=$(echo "$i" | sed 's/:/_/g')
+    #echo mv \"$i\" \"$j\"
+    mv "$i" "$j"
+done
